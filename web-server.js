80eeec279d2b73e13f3c294088aeaa1337797472
pkrawat1 <pankajrawat19sept@gmail.com>
2013-07-31 10:33:10 +0530

    updated ppt

--- web-server.js
+++ web-server.js
@@ -1,9 +1,9 @@
 var http = require('http');
 
 http.createServer(function(request, response) {
-        response.writeHead(200);
-            response.write("Hello");
-                setTimeout(function(){
-                            response.end("World");
-                                }, 5000);
+    response.writeHead(200);        
+    response.write("Hello");
+    setTimeout(function(){
+        response.end("World");                        
+    }, 5000);
 }).listen(8080);
